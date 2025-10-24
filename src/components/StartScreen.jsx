6a0d00a554d3bb568d3e629600d9e38a2be3a99d
cortezinho <gabrielcortez2802@gmail.com>
2025-10-24 19:35:19 -0300

    pokemons alterados

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,9 +1,9 @@
 // src/components/StartScreen.jsx
-import React from 'react';
-import PressStartTitle from './PressStartTitle';
-import Character from './Character';
-import images from '../data/images';
-import backgrounds from '../assets/backgrounds/intro-bg.png';
+import React from "react";
+import PressStartTitle from "./PressStartTitle";
+import Character from "./Character";
+import images from "../data/images";
+import backgrounds from "../assets/backgrounds/intro-bg2.png";
 
 function StartScreen({ onStart }) {
   return (
@@ -13,10 +13,10 @@ function StartScreen({ onStart }) {
       onClick={onStart}
     >
       <PressStartTitle />
-      <Character src={images.squirtle} alt="Squirtle" className="character squirtle flip-right" />
+      {/* <Character src={images.squirtle} alt="Squirtle" className="character squirtle flip-right" />
       <Character src={images.bulbasaur} alt="Bulbasaur" className="bulbasaur flip-right" />
       <Character src={images.charmander} alt="Charmander" className="charmander flip-right" />
-      <Character src={images.pikachu} alt="Pikachu" className="pikachu" />
+      <Character src={images.pikachu} alt="Pikachu" className="pikachu" /> */}
     </div>
   );
 }
